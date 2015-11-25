98f8cf616acf6d1f9c3fa0c65187805d558125df
i.dedic <i.dedic@levi9.com>
2015-11-25 16:07:53 +0100

    Initial

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.Dynamic;
+using WebApplication1.Models;
+using System.Data.SQLite;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonNetResult b()
+        {
+            dynamic cResponse = new ExpandoObject();
+            cResponse.Property1 = "value1";
+            cResponse.Property2 = "value2";
+            return new JsonNetResult { Data = cResponse };
+        }
+
+        public JsonNetResult Query(string q)
+        {
+            try
+            {
+                var db = DB.getDb();
+                db.Open();
+
+                SQLiteCommand c = new SQLiteCommand(q, db);
+                SQLiteDataReader r = c.ExecuteReader();
+
+                var responseData = new List<dynamic>();
+                var columns = Enumerable.Range(0, r.FieldCount).Select(r.GetName).ToList();
+                while (r.Read())
+                {
+                    var map = new ExpandoObject() as IDictionary<string, Object>;
+                    foreach (var column in columns)
+                    {
+                        map.Add(column, r[column]);
+                    }
+                    responseData.Add(map);
+                }
+
+                db.Close();
+                return new JsonNetResult { Data = responseData };
+
+            }
+            catch (Exception e)
+            {
+                return new JsonNetResult { Data = new { Error = true, Message = e.Message } };
+            }
+            
+        }
+
+    }
+}
---
+++ WebApplication1/Models/DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class DB
+    {
+        //static SQLiteConnection db;
+
+        //static DB()
+        //{
+        //    var cs = getConnectionString();
+        //    db = new SQLiteConnection(cs);
+        //}
+
+        public static SQLiteConnection getDb()
+        {
+            var cs = getConnectionString();
+            return new SQLiteConnection(cs);
+        }
+
+        private static string getConnectionString()
+        {
+            string relativePath = @"App_Data\test.sqlite";
+            string currentPath;
+            string absolutePath;
+            string connectionString;
+
+            currentPath = AppDomain.CurrentDomain.BaseDirectory;
+            absolutePath = Path.Combine(currentPath, relativePath);
+
+            connectionString = string.Format("DataSource={0};Version=3;", absolutePath);
+            connectionString = connectionString.Replace("\\", "/");
+            return connectionString;
+        }
+    }
+}
