4943b4994acab36e1bf61d8b15bbc46b4a026257
dedabyte <zwerrr@gmail.com>
2016-01-28 22:42:18 +0100

    Error handling: login.

--- Client/scripts/app/auth/auth-directive.js
+++ Client/scripts/app/auth/auth-directive.js
@@ -13,10 +13,13 @@
           '<form class="login-form" ng-submit="snAuth.login()">' +
           '<p><input id="user" type="text" placeholder="Username" ng-model="snAuth.user" autofocus/></p>' +
           '<p><input id="pass" type="password" placeholder="Password" ng-model="snAuth.pass"/></p>' +
+          '<p class="login-error" ng-bind="snAuth.errorMessage" ng-show="snAuth.errorMessage"></p>' +
           '<p><button type="submit" ng-disabled="!snAuth.user || !snAuth.pass">Login</button></p>' +
           '</form>',
-        controller: function(){
+        controller: function($scope){
           var self = this;
+
+          self.errorMessage = '';
 
           function login(){
             $.ajax({
@@ -29,10 +32,20 @@
               success: function (response) {
                 if (response.error) {
                   console.error(response.message);
+                  $scope.$evalAsync(function(){
+                    self.errorMessage = response.message;
+                  });
                   return;
                 }
+                self.errorMessage = '';
                 localStorage.setItem('sn-token', response.data.token);
                 location.reload(true);
+              },
+              error: function(error){
+                console.error(error);
+                $scope.$evalAsync(function(){
+                  self.errorMessage = 'Error. See console for more info.';
+                });
               }
             });
           }
